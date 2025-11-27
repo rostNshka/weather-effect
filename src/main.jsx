ef3f609653217e2f963eeaae6ecac41735636709
RostNshka <devkotder@ya.ru>
2025-11-27 21:26:04 +0400

    add Section in main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Section from "./sections/Section/index.js"
+import './styles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <Section />
   </StrictMode>,
 )
